32224e3b7cd78aba44f35050e64d2ff66b490713
IDystopiaI <dylan.london94@gmail.com>
2024-03-05 13:32:38 -0800

    example showing how to use node to host content from a server

---
+++ 2-1_server_side_js/index.js
@@ -0,0 +1,11 @@
+// basically an import statement for Node
+const express = require("express");
+// create an express instance
+const app = express();
+
+// start listening for clients
+app.listen(3000, () => console.log("listening at port 3000"));
+
+// point to a folder containing html, css and js
+app.use(express.static("public"));
+
